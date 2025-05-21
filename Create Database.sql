d357d9276b1abb4657ec983390d9e643e5cd06f5
Lillian <le732943@gmail.com>
2025-05-21 16:18:58 -0600

    Create Create Database.sql

---
+++ Create Database.sql
@@ -0,0 +1,14 @@
+-- Create Database
+---------------------------------------------------------------------
+
+-- 1. Connect to your SQL Server instance, master database
+
+-- 2. Run the following code to create an empty database called SQLDB
+USE master;
+
+-- Create database
+CREATE DATABASE SQLDB;
+GO
+
+USE SQLDB;
+GO
