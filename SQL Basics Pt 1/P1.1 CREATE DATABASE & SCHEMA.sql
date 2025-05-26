faf65e4046cd3b32ffc6872dfbd8cb7173edf850
Lillian <le732943@gmail.com>
2025-05-26 09:53:30 -0600

    Update P1.1 CREATE DATABASE & SCHEMA.sql

--- SQL Basics Pt 1/P1.1 CREATE DATABASE & SCHEMA.sql
+++ SQL Basics Pt 1/P1.1 CREATE DATABASE & SCHEMA.sql
@@ -1,17 +1,20 @@
-----------------------------------------------------------------
--- Create a new database called 'TestDatabase'
--- GO seperates out different queries from each other and
--- will run all queries individually
-----------------------------------------------------------------
-USE master
+-- ----------------------------------------------------------------
+-- Author: Lillian ELek
+-- Create date: 2025-05-25
+-- Description: Create a new database called 'TestDatabase'
+-- Create a new schema called 'Inventory'
+-- ----------------------------------------------------------------
+ USE master
 
 DROP DATABASE IF EXISTS TestDatabase
 CREATE DATABASE TestDatabase
 
 GO
-----------------------------------------------------------------
+-- GO seperates out different queries from each other and
+-- will run all queries individually
+
 -- Create a new schema called 'Inventory'
-----------------------------------------------------------------
+
 USE TestDatabase
 GO
 
