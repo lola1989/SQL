2bbc070bb738dd7f3b98ed92910fe1714fc0a046
Lillian <le732943@gmail.com>
2025-05-27 14:13:03 -0600

    Update P3.3 GROUP BY & Aliases.sql

--- SQL Basics Pt 3/P3.3 GROUP BY & Aliases.sql
+++ SQL Basics Pt 3/P3.3 GROUP BY & Aliases.sql
@@ -41,6 +41,33 @@ FROM Production.Product
 GROUP BY ReorderPoint
 ORDER BY ReorderPoint
 
+-- Query to show the Alis on all column names
+SELECT [Product].[ProductID],
+[Product].[Name],
+[Product].[ProductNumber],
+[Product].[MakeFlag],
+[Product].[FinishedGoodsFlag],
+[Product].[Color],
+[Product].[SafetyStockLevel],
+[Product].[ReorderPoint],
+[Product].[StandardCost],
+[Product].[ListPrice],
+[Product].[Size],
+[Product].[SizeUnitMeasureCode],
+[Product].[WeightUnitMeasureCode],
+[Product].[Weight],
+[Product].[DaysToManufacture],
+[Product].[ProductLine],
+[Product].[Class],
+[Product].[Style],
+[Product].[ProductSubcategoryID],
+[Product].[ProductModelID],
+[Product].[SellStartDate],
+[Product].[SellEndDate],
+[Product].[DiscontinuedDate],
+[Product].[rowguid],
+[Product].[ModifiedDate] FROM Production.Product AS Product
+
 SELECT [ProductID],
     [Name],
     [ProductNumber],
